f5a044d1573779b884388738dda4d2912486556e
tsve0003 <65698849+tsve0003@users.noreply.github.com>
2025-11-05 13:08:15 -0500

    Delete G7 Projects/script.js

--- G7 Projects/script.js
+++
@@ -1,8 +0,0 @@
-// Placeholder for future interactivity.
-document.querySelectorAll("details").forEach(d => {
-  d.addEventListener("toggle", () => {
-    if (d.open) {
-      document.querySelectorAll("details").forEach(o => { if (o !== d) o.open = false; });
-    }
-  });
-});
